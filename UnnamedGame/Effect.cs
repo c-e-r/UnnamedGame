b477f5a1e72be66cd521bd2893c74b01b7f198d6
Cameron Roberts <cameronroberts@shaw.ca>
2018-07-11 20:26:18 -0700

    Add effect class

---
+++ UnnamedGame/Effect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnnamedGame
+{
+    abstract class Effect
+    {
+        protected String id;
+        protected String name;
+
+        enum Desc { Activate, Resist }
+
+        protected int acc;
+        protected int resistType;
+
+        protected int result;
+
+        protected int duration;
+        protected int increment;
+
+        protected List<Effect> children;
+
+
+        public Effect()
+        {
+
+        }
+
+
+        abstract public void Activate();
+
+
+        abstract public void Update();
+
+    }
+}
